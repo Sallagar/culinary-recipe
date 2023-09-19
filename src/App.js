f6f825fcb02eaac8e85c54f994e5db2b40f1b410
Ivan <ivankononenko772@gmail.com>
2023-09-19 08:52:43 +0300

    basic routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import './App.css';
+import {BrowserRouter as Routes, Route, Switch} from 'react-router-dom'
+import {Header} from './components/Header'
+import {Footer} from './components/Footer'
+import { Home } from './pages/Home';
+import { About } from './pages/About';
+import { Contact } from './pages/Contact';
+import { NotFound } from './pages/NotFound';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Let's start!!</h1>
-    </div>
+    <>
+      <Header />
+      <main className='container content'>
+        <Routes>
+          <Switch>
+            <Route exact path='/' render={ () => <Home /> }/>
+            <Route path='/about' render={ () => <About /> }/> 
+            <Route path='/contact' render={ () => <Contact /> }/>
+            <Route render={NotFound} />
+          </Switch>
+        </Routes> 
+      </main>
+      <Footer />
+    </>
   );
 }
 
